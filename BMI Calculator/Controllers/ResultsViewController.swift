8650f17b536a72fc8ff6a05ca828f917abb7acbb
Ethan Smith <edhsmith@icloud.com>
2021-02-22 21:48:19 +1300

    Added final calculation view with updated results. Ensured correct calculation.

--- BMI Calculator/Controllers/ResultsViewController.swift
+++ BMI Calculator/Controllers/ResultsViewController.swift
@@ -12,13 +12,18 @@ class ResultsViewController: UIViewController {
     
     @IBOutlet weak var bmiLabel: UILabel!
     @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet var mainView: UIView!
     
     var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
     
     override func viewDidLoad() {
         // Check view loaded, update bmiLabel with calculated bmiValue
         super.viewDidLoad()
         bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        mainView.backgroundColor = color
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
